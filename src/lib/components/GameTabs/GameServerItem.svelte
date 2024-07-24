687183f97dd6bca832a0d2f26e42b22a31e9a16f
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-07-24 22:37:15 +0300

    style: fixed tabs

--- src/lib/components/GameTabs/GameServerItem.svelte
+++ src/lib/components/GameTabs/GameServerItem.svelte
@@ -1,68 +1,68 @@
 <script lang="ts">
-	import { robloxApi } from "$lib/robloxApi";
-	import type { GameServer } from "$lib/typings";
+    import { robloxApi } from "$lib/robloxApi";
+    import type { GameServer } from "$lib/typings";
 
-	export let gameServer: GameServer;
-	export let avatarUrls: Record<string, string>;
-	export let placeId: number;
+    export let gameServer: GameServer;
+    export let avatarUrls: Record<string, string>;
+    export let placeId: number;
 
-	function join() {
-		robloxApi.playServer(placeId, gameServer.id);
-	}
+    function join() {
+        robloxApi.playServer(placeId, gameServer.id);
+    }
 </script>
 
 <div class="game-server">
-	<div class="game-server-avatars">
-		{#each gameServer.player_tokens as playerToken}
-			{#if avatarUrls[playerToken]}
-				<img
-					class="avatar-image"
-					src={avatarUrls[playerToken]}
-					alt="Player Headshot"
-				/>
-			{:else}
-				<div
-					class="placeholder avatar-image w-full h-full aspect-square"
-				/>
-			{/if}
-		{/each}
+    <div class="game-server-avatars">
+        {#each gameServer.player_tokens as playerToken}
+            {#if avatarUrls[playerToken]}
+                <img
+                    class="avatar-image"
+                    src={avatarUrls[playerToken]}
+                    alt="Player Headshot"
+                />
+            {:else}
+                <div
+                    class="placeholder avatar-image w-full h-full aspect-square"
+                />
+            {/if}
+        {/each}
 
-		{#if gameServer.playing > 5}
-			<div class="w-full h-full flex justify-center items-center">
-				<p class="text-2xl font-bold">+{gameServer.playing - 5}</p>
-			</div>
-		{/if}
-	</div>
-	<p>{gameServer.playing} of {gameServer.max_players}</p>
+        {#if gameServer.playing > 5}
+            <div class="w-full h-full flex justify-center items-center">
+                <p class="text-2xl font-bold">+{gameServer.playing - 5}</p>
+            </div>
+        {/if}
+    </div>
+    <p>{gameServer.playing} of {gameServer.max_players}</p>
 
-	<div class="game-server-join">
-		<button on:click={join}>Join</button>
-	</div>
+    <div class="game-server-join">
+        <button on:click={join}>Join</button>
+    </div>
 </div>
 
 <style lang="scss">
-	.game-server {
-		@apply p-2;
-		@apply bg-[#181818] rounded-lg;
+    .game-server {
+        @apply p-2;
+        @apply bg-[#181818] rounded-lg;
 
-		.game-server-avatars {
-			@apply grid grid-cols-3 justify-items-center gap-2;
-			@apply mb-2;
+        .game-server-avatars {
+            @apply grid grid-cols-3 justify-items-center gap-2;
+            @apply mb-2;
 
-			.avatar-image {
-				@apply rounded-md;
-			}
+            .avatar-image {
+                @apply rounded-md;
+            }
 
-			img.avatar-image {
-				@apply bg-[#282828];
-			}
-		}
+            img.avatar-image {
+                @apply bg-[#282828];
+            }
+        }
 
-		.game-server-join {
-			button {
-				@apply px-2 py-1 w-full;
-				@apply bg-green-600 rounded-md;
-			}
-		}
-	}
+        .game-server-join {
+            button {
+                @apply px-2 py-1 w-full;
+                @apply bg-green-600 rounded-md;
+            }
+        }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,239 +1,239 @@
 <script lang="ts">
-	import { robloxApi } from "$lib/robloxApi";
-	import chunk from "lodash.chunk";
+    import { robloxApi } from "$lib/robloxApi";
+    import chunk from "lodash.chunk";
 
-	import FriendCard from "$lib/components/Cards/FriendCard.svelte";
-	import FriendCardSkeleton from "$lib/components/Cards/FriendCardSkeleton.svelte";
-	import GameCard from "$lib/components/Cards/GameCard.svelte";
-	import GameCardSkeleton from "$lib/components/Cards/GameCardSkeleton.svelte";
+    import FriendCard from "$lib/components/Cards/FriendCard.svelte";
+    import FriendCardSkeleton from "$lib/components/Cards/FriendCardSkeleton.svelte";
+    import GameCard from "$lib/components/Cards/GameCard.svelte";
+    import GameCardSkeleton from "$lib/components/Cards/GameCardSkeleton.svelte";
 
-	import {
-		TreatmentType,
-		ThumbnailSize,
-		ThumbnailType,
-		type InternalFriend,
-		type RecommendationsTopic,
-	} from "$lib/typings.js";
+    import {
+        TreatmentType,
+        ThumbnailSize,
+        ThumbnailType,
+        type InternalFriend,
+        type RecommendationsTopic,
+    } from "$lib/typings.js";
 
-	let friends: InternalFriend[] = [];
-	let topics: RecommendationsTopic[] = [];
-	let gameIcons: Record<number, string> = {};
+    let friends: InternalFriend[] = [];
+    let topics: RecommendationsTopic[] = [];
+    let gameIcons: Record<number, string> = {};
 
-	const PRESENCE_INDEXES: Record<string, number> = {
-		Invisible: 0,
-		Offline: 0,
-		Online: 1,
-		InGame: 2,
-		InStudio: 2,
-	};
+    const PRESENCE_INDEXES: Record<string, number> = {
+        Invisible: 0,
+        Offline: 0,
+        Online: 1,
+        InGame: 2,
+        InStudio: 2,
+    };
 
-	async function fetchFriends() {
-		let friendsArray = await robloxApi.getFriendsList();
+    async function fetchFriends() {
+        let friendsArray = await robloxApi.getFriendsList();
 
-		friendsArray = friendsArray.sort(
-			(a, b) =>
-				PRESENCE_INDEXES[b.presence_type] -
-				PRESENCE_INDEXES[a.presence_type],
-		);
+        friendsArray = friendsArray.sort(
+            (a, b) =>
+                PRESENCE_INDEXES[b.presence_type] -
+                PRESENCE_INDEXES[a.presence_type],
+        );
 
-		const [friendsPresencesArray, friendsHeadshotsArray] =
-			await Promise.all([
-				robloxApi.getPresences(friendsArray.map((fr) => fr.user_id)),
-				robloxApi.getThumbnailsUrls(
-					friendsArray.map((fr) => fr.user_id),
-					ThumbnailSize.S150x150,
-					ThumbnailType.AvatarHeadshot,
-				),
-			]);
+        const [friendsPresencesArray, friendsHeadshotsArray] =
+            await Promise.all([
+                robloxApi.getPresences(friendsArray.map((fr) => fr.user_id)),
+                robloxApi.getThumbnailsUrls(
+                    friendsArray.map((fr) => fr.user_id),
+                    ThumbnailSize.S150x150,
+                    ThumbnailType.AvatarHeadshot,
+                ),
+            ]);
 
-		friends = friendsArray.map((info, i) => ({
-			info,
-			headshot: friendsHeadshotsArray[i],
-			presence: friendsPresencesArray[i],
-		}));
-	}
+        friends = friendsArray.map((info, i) => ({
+            info,
+            headshot: friendsHeadshotsArray[i],
+            presence: friendsPresencesArray[i],
+        }));
+    }
 
-	async function fetchRecommendations() {
-		topics = await robloxApi
-			.getRecommendations()
-			.then((topics) =>
-				topics
-					.filter(
-						(a) =>
-							a.treatment_type !== TreatmentType.FriendCarousel,
-					)
-					.map((a) => {
-						if (a.topic_id === 100000003) {
-							a.recommendation_list = a.recommendation_list.slice(
-								0,
-								8,
-							);
-						}
+    async function fetchRecommendations() {
+        topics = await robloxApi
+            .getRecommendations()
+            .then((topics) =>
+                topics
+                    .filter(
+                        (a) =>
+                            a.treatment_type !== TreatmentType.FriendCarousel,
+                    )
+                    .map((a) => {
+                        if (a.topic_id === 100000003) {
+                            a.recommendation_list = a.recommendation_list.slice(
+                                0,
+                                8,
+                            );
+                        }
 
-						return a;
-					})
-					.sort((a, b) =>
-						a.topic_id === 100000003
-							? -1
-							: b.topic_id === 100000003
-								? 1
-								: 0,
-					),
-			)
-			.catch((err) => {
-				console.error(err);
+                        return a;
+                    })
+                    .sort((a, b) =>
+                        a.topic_id === 100000003
+                            ? -1
+                            : b.topic_id === 100000003
+                              ? 1
+                              : 0,
+                    ),
+            )
+            .catch((err) => {
+                console.error(err);
 
-				return [];
-			});
+                return [];
+            });
 
-		const iconsBatches = await Promise.all(
-			topics.map(async (topic) => {
-				if (topic.treatment_type === TreatmentType.Carousel) {
-					const thumbnails = (
-						await Promise.all(
-							chunk(
-								topic.recommendation_list.map(
-									(r) => r.universe_id,
-								),
-								9,
-							).map((universeIdsChunk) =>
-								robloxApi.getThumbnailsUrls(
-									universeIdsChunk,
-									ThumbnailSize.S150x150,
-									ThumbnailType.GameIcon,
-								),
-							),
-						)
-					).flat(1);
+        const iconsBatches = await Promise.all(
+            topics.map(async (topic) => {
+                if (topic.treatment_type === TreatmentType.Carousel) {
+                    const thumbnails = (
+                        await Promise.all(
+                            chunk(
+                                topic.recommendation_list.map(
+                                    (r) => r.universe_id,
+                                ),
+                                9,
+                            ).map((universeIdsChunk) =>
+                                robloxApi.getThumbnailsUrls(
+                                    universeIdsChunk,
+                                    ThumbnailSize.S150x150,
+                                    ThumbnailType.GameIcon,
+                                ),
+                            ),
+                        )
+                    ).flat(1);
 
-					return topic.recommendation_list.map((r, i) => [
-						r.universe_id,
-						thumbnails[i],
-					]);
-				} else {
-					const thumbnails = (
-						await Promise.all(
-							chunk(
-								topic.recommendation_list.map(
-									(r) => r.root_place_id,
-								),
-								9,
-							).map((universeIdsChunk) =>
-								robloxApi.getThumbnailsUrls(
-									universeIdsChunk,
-									ThumbnailSize.S768x432,
-									ThumbnailType.GameThumbnail,
-								),
-							),
-						)
-					).flat(1);
+                    return topic.recommendation_list.map((r, i) => [
+                        r.universe_id,
+                        thumbnails[i],
+                    ]);
+                } else {
+                    const thumbnails = (
+                        await Promise.all(
+                            chunk(
+                                topic.recommendation_list.map(
+                                    (r) => r.root_place_id,
+                                ),
+                                9,
+                            ).map((universeIdsChunk) =>
+                                robloxApi.getThumbnailsUrls(
+                                    universeIdsChunk,
+                                    ThumbnailSize.S768x432,
+                                    ThumbnailType.GameThumbnail,
+                                ),
+                            ),
+                        )
+                    ).flat(1);
 
-					return topic.recommendation_list.map((r, i) => [
-						r.universe_id,
-						thumbnails[i],
-					]);
-				}
-			}),
-		);
+                    return topic.recommendation_list.map((r, i) => [
+                        r.universe_id,
+                        thumbnails[i],
+                    ]);
+                }
+            }),
+        );
 
-		gameIcons = Object.fromEntries(iconsBatches.flat(1));
-	}
+        gameIcons = Object.fromEntries(iconsBatches.flat(1));
+    }
 </script>
 
 <section class="friends">
-	<p class="section-title">Friends</p>
-	<div class="section-content">
-		{#await fetchFriends()}
-			{#each Array(8).map(() => 0) as _}
-				<FriendCardSkeleton />
-			{/each}
-		{:then}
-			{#each friends as friend}
-				<FriendCard {friend} />
-			{/each}
-		{/await}
-	</div>
+    <p class="section-title">Friends</p>
+    <div class="section-content">
+        {#await fetchFriends()}
+            {#each Array(8).map(() => 0) as _}
+                <FriendCardSkeleton />
+            {/each}
+        {:then}
+            {#each friends as friend}
+                <FriendCard {friend} />
+            {/each}
+        {/await}
+    </div>
 </section>
 
 {#await fetchRecommendations()}
-	{#each Array(8).map(() => 0) as _}
-		<section class="topic-section">
-			<p class="section-title"><span class="placeholder w-28" /></p>
-			<div class="section-content carousel">
-				{#each Array(10).map(() => 0) as _}
-					<GameCardSkeleton />
-				{/each}
-			</div>
-		</section>
-	{/each}
+    {#each Array(8).map(() => 0) as _}
+        <section class="topic-section">
+            <p class="section-title"><span class="placeholder w-28" /></p>
+            <div class="section-content carousel">
+                {#each Array(10).map(() => 0) as _}
+                    <GameCardSkeleton />
+                {/each}
+            </div>
+        </section>
+    {/each}
 {:then}
-	{#each topics as topic}
-		<section class="topic-section">
-			<p class="section-title">{topic.topic}</p>
-			{#if topic.subtitle}
-				<p class="section-subtitle">
-					{topic.subtitle}
-				</p>
-			{/if}
+    {#each topics as topic}
+        <section class="topic-section">
+            <p class="section-title">{topic.topic}</p>
+            {#if topic.subtitle}
+                <p class="section-subtitle">
+                    {topic.subtitle}
+                </p>
+            {/if}
 
-			<div
-				class={topic.treatment_type === TreatmentType.SortlessGrid
-					? "section-content sortless-grid"
-					: "section-content carousel"}
-			>
-				{#each topic.recommendation_list as game}
-					<GameCard
-						{game}
-						thumbnail={gameIcons[game.universe_id]}
-						treatmentType={topic.treatment_type}
-					/>
-				{/each}
-			</div>
-		</section>
-	{/each}
+            <div
+                class={topic.treatment_type === TreatmentType.SortlessGrid
+                    ? "section-content sortless-grid"
+                    : "section-content carousel"}
+            >
+                {#each topic.recommendation_list as game}
+                    <GameCard
+                        {game}
+                        thumbnail={gameIcons[game.universe_id]}
+                        treatmentType={topic.treatment_type}
+                    />
+                {/each}
+            </div>
+        </section>
+    {/each}
 {/await}
 
 <style lang="scss">
-	section {
-		.section-subtitle {
-			@apply text-xs text-[#C0C0C0];
-		}
+    section {
+        .section-subtitle {
+            @apply text-xs text-[#C0C0C0];
+        }
 
-		.section-content {
-			@apply border border-[#787878] bg-[#121212];
-			@apply p-2 rounded-lg;
-		}
-	}
+        .section-content {
+            @apply border border-[#787878] bg-[#121212];
+            @apply p-2 rounded-lg;
+        }
+    }
 
-	section.friends {
-		.section-content {
-			@apply flex gap-2;
-			@apply overflow-scroll;
-		}
-	}
+    section.friends {
+        .section-content {
+            @apply flex gap-2;
+            @apply overflow-scroll;
+        }
+    }
 
-	section.topic-section {
-		@apply my-2;
+    section.topic-section {
+        @apply my-2;
 
-		.section-content {
-			@apply gap-2;
-			@apply overflow-scroll;
+        .section-content {
+            @apply gap-2;
+            @apply overflow-scroll;
 
-			&.carousel {
-				@apply flex;
-			}
+            &.carousel {
+                @apply flex;
+            }
 
-			&.sortless-grid {
-				@media screen and (max-width: 400px) {
-					@apply grid-cols-1;
-				}
+            &.sortless-grid {
+                @media screen and (max-width: 400px) {
+                    @apply grid-cols-1;
+                }
 
-				@media screen and (max-width: 800px) {
-					@apply grid-cols-2;
-				}
+                @media screen and (max-width: 800px) {
+                    @apply grid-cols-2;
+                }
 
-				@apply grid md:grid-cols-3 lg:grid-cols-4;
-			}
-		}
-	}
+                @apply grid md:grid-cols-3 lg:grid-cols-4;
+            }
+        }
+    }
 </style>

8b236c3e49a0f99a5bf8d9c49c4012788ae93997
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-07-25 09:28:13 +0300

    feat: Added button to copy roblox server link (in friends' tooltip)

--- src/lib/components/Cards/FriendTooltip.svelte
+++ src/lib/components/Cards/FriendTooltip.svelte
@@ -6,6 +6,7 @@
         UserPresence,
         PlaceDetails,
     } from "$lib/typings.ts";
+    import { writeText } from "@tauri-apps/api/clipboard";
 
     export let friend: FriendUserInformation;
     export let friendPresence: UserPresence;
@@ -20,6 +21,21 @@
             );
         }
     }
+
+    let copied = false;
+
+    async function copyLink() {
+        if (friendPresence.place_id && friendPresence.game_id) {
+            await writeText(
+                `roblox://experiences/start?placeId=${friendPresence.place_id}&gameInstanceId=${friendPresence.game_id}`,
+            );
+
+            copied = true;
+            setTimeout(() => {
+                copied = false;
+            }, 2000);
+        }
+    }
 </script>
 
 <div class="friend-tooltip" data-user-id={friend.user_id}>
@@ -29,7 +45,44 @@
 
             <div class="friend-tooltip-footer">
                 <p>{placeDetails?.name ?? "Unknown"}</p>
-                <button class="join-button" on:click={join}>Join</button>
+                <div class="join-button-container">
+                    <button class="join-button" on:click={join}>Join</button>
+                    <button class="copy-button" on:click={copyLink}>
+                        {#if copied}
+                            <svg
+                                class="w-6 h-6"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M9 2a1 1 0 0 0-1 1H6a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-2a1 1 0 0 0-1-1H9Zm1 2h4v2h1a1 1 0 1 1 0 2H9a1 1 0 0 1 0-2h1V4Zm5.707 8.707a1 1 0 0 0-1.414-1.414L11 14.586l-1.293-1.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        {:else}
+                            <svg
+                                class="w-6 h-6"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-6 8a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        {/if}
+                    </button>
+                </div>
             </div>
         </div>
     {/if}
@@ -86,9 +139,18 @@
 
             @apply bg-gradient-to-t from-black to-white/0;
 
-            .join-button {
-                @apply px-2 py-1 w-full;
-                @apply bg-green-600 rounded-sm;
+            .join-button-container {
+                @apply flex gap-2;
+
+                .join-button,
+                .copy-button {
+                    @apply px-2 py-1;
+                    @apply bg-green-600 rounded-sm;
+                }
+
+                .join-button {
+                    @apply flex-grow;
+                }
             }
         }
     }

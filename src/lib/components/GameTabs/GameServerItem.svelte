76f3c82d00e337b4c00db625a492a42449bc8a04
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-07-25 09:32:13 +0300

    feat: added copy link to `GameServerItem`

--- src/lib/components/GameTabs/GameServerItem.svelte
+++ src/lib/components/GameTabs/GameServerItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { robloxApi } from "$lib/robloxApi";
     import type { GameServer } from "$lib/typings";
+    import { writeText } from "@tauri-apps/api/clipboard";
 
     export let gameServer: GameServer;
     export let avatarUrls: Record<string, string>;
@@ -8,6 +9,19 @@
 
     function join() {
         robloxApi.playServer(placeId, gameServer.id);
+    }
+
+    let copied = false;
+
+    async function copyLink() {
+        await writeText(
+            `roblox://experiences/start?placeId=${placeId}&gameInstanceId=${gameServer.id}`,
+        );
+
+        copied = true;
+        setTimeout(() => {
+            copied = false;
+        }, 2000);
     }
 </script>
 
@@ -35,8 +49,44 @@
     </div>
     <p>{gameServer.playing} of {gameServer.max_players}</p>
 
-    <div class="game-server-join">
-        <button on:click={join}>Join</button>
+    <div class="game-server-join-container">
+        <button class="game-server-join" on:click={join}>Join</button>
+
+        <button class="game-server-copy" on:click={copyLink}>
+            {#if copied}
+                <svg
+                    class="w-6 h-6"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M9 2a1 1 0 0 0-1 1H6a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-2a1 1 0 0 0-1-1H9Zm1 2h4v2h1a1 1 0 1 1 0 2H9a1 1 0 0 1 0-2h1V4Zm5.707 8.707a1 1 0 0 0-1.414-1.414L11 14.586l-1.293-1.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            {:else}
+                <svg
+                    class="w-6 h-6"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-6 8a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            {/if}
+        </button>
     </div>
 </div>
 
@@ -57,11 +107,17 @@
                 @apply bg-[#282828];
             }
         }
+        .game-server-join-container {
+            @apply flex gap-2;
 
-        .game-server-join {
-            button {
-                @apply px-2 py-1 w-full;
+            .game-server-join,
+            .game-server-copy {
+                @apply px-2 py-1;
                 @apply bg-green-600 rounded-md;
+            }
+
+            .game-server-join {
+                @apply flex-grow;
             }
         }
     }

8a2fdcf85d3589f11ebe96b01919e5dba879fb5d
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-09-11 16:43:10 +0300

    fix: /settings -> /settings/about

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-    import * as Section from "@ui/section";
-</script>
+    import { goto } from "$app/navigation";
 
-<Section.Root>
-    <Section.Title>Hello</Section.Title>
-    <Section.Content
-        >This menu is WIP, probably Ill port here settings options from Roblox
-        website</Section.Content
-    >
-</Section.Root>
+    goto("/settings/about");
+</script>

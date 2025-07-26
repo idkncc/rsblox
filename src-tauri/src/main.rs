0e486dd120d0d5b4711df12e11d97573ba236077
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2025-07-26 19:12:14 +0300

    initial commit of rewrite.
    tauri and svelte had major updates.
    also rust code structure could be much better.
    
    so i'm rewriting project :/

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod client;
-mod commands;
-mod roblox_api;
-mod types;
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(roblox_api::init())
-        .invoke_handler(tauri::generate_handler![])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    rsblox_lib::run()
 }
